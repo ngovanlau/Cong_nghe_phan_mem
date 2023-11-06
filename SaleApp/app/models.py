192b5ead16c5e1db59328b6650e030a1b9320808
ngovanlau <ngovanlau2003@gmail.com>
2023-11-06 09:25:14 +0700

    ngay 06/11/2023

---
+++ SaleApp/app/models.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Integer, Column, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app import db, app
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=True)
+    products = relationship('Product', backref='category', lazy=True)
+
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=True)
+    price = Column(Float, default=0)
+    image = Column(String(100), default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+    active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+if __name__ == '__main__':
+    with app.app_context():
+        # db.create_all()
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        # c3 = Category(name='Desktop')
+        # db.session.add(c1)
+        # db.session.add(c2)
+        # db.session.add(c3)
+        # db.session.commit()
+
+        # p1 = Product(name='iPhone 13', price=20000000, category_id=1)
+        # p2 = Product(name='Galaxy S23', price=24999999, category_id=1)
+        # p3 = Product(name='Ipad', price=45300000, category_id=2)
+        # p4 = Product(name='MSI', price=32300000, category_id=3)
+        # p5 = Product(name='Oppo N3 Flip', price=20333000, category_id=1)
+        # p6 = Product(name='Galaxy Tab', price=1203000, category_id=2)
+        # db.session.add(p1)
+        # db.session.add(p2)
+        # db.session.add(p3)
+        # db.session.add(p4)
+        # db.session.add(p5)
+        # db.session.add(p6)
+        # db.session.commit()
