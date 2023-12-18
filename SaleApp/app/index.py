edc1ee1437ede5a907c391acf9f18c3ca5539058
ngovanlau <ngovanlau2003@gmail.com>
2023-12-18 09:29:52 +0700

    ngay 18/12/2023

--- SaleApp/app/index.py
+++ SaleApp/app/index.py
@@ -1,5 +1,5 @@
-from flask import render_template, request, redirect
-import dao
+from flask import render_template, request, redirect, jsonify, session
+import dao, utils
 from app import app, login
 from flask_login import login_user
 
@@ -19,6 +19,49 @@ def index():
 @app.route("/hello/<name>")
 def hello(name):
     return render_template('index.html', message="XIN CHAO %s!!!" % name)
+
+
+@app.route('/api/cart', methods=['post'])
+def add_to_cart():
+    data = request.json
+
+    cart = session.get('cart')
+    if cart is None:
+        cart = {}
+
+    id = str(data.get('id'))
+
+    if id in cart:
+        cart[id]['quantity'] += 1
+    else:
+        cart[id] = {
+            "id": id,
+            "name": data.get('name'),
+            "price": data.get('price'),
+            "quantity": 1
+        }
+
+    session['cart'] = cart
+    print(cart)
+    """
+        {
+            "1": {
+                "id": "1",
+                "name": "...",
+                "price": 1,
+                "quantity": 2
+            }, 
+            "2": {
+                "id": "2",
+                "name": "...",
+                "price": 1,
+                "quantity": 1
+            },
+        }
+    """
+
+    return jsonify(utils.count_cart(cart))
+
 
 @app.route('/admin/login', methods=['post'])
 def login_admin():
