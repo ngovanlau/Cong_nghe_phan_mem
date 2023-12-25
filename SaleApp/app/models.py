8304390988e2331afc4d2b5f6781dfc7a25e87cd
ngovanlau <ngovanlau2003@gmail.com>
2023-12-25 11:33:54 +0700

    25/12/2023

--- SaleApp/app/models.py
+++ SaleApp/app/models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Integer, Column, String, Float, Boolean, ForeignKey, Enum
+from sqlalchemy import Integer, Column, String, Float, Boolean, ForeignKey, Enum, DateTime
 from sqlalchemy.orm import relationship
 from app import db, app
 from flask_login import UserMixin
 import enum
+from datetime import datetime
 
 
 class UserRoleEnum(enum.Enum):
@@ -18,7 +19,7 @@ class User(db.Model, UserMixin):
     avatar = Column(String(100),
                     default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
     user_role = Column(Enum(UserRoleEnum), default=UserRoleEnum.USER)
-
+    receipt = relationship('Receipt', backref='user', lazy=True)
 
     def __str__(self):
         return self.name
@@ -43,38 +44,59 @@ class Product(db.Model):
                    default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
     active = Column(Boolean, default=True)
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+    receipt_details = relationship('ReceiptDetails', backref='product', lazy=True)
 
     def __str__(self):
         return self.name
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    create_date = Column(DateTime, default=datetime.now())
+    active = Column(Boolean, default=True)
+
+
+class Receipt(BaseModel):
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    receipt_details = relationship('ReceiptDetails', backref='receipt', lazy=True)
+
+
+class ReceiptDetails(BaseModel):
+    quantity = Column(Integer, default=0)
+    price = Column(Float, default=0)
+    receipt_id = Column(Integer, ForeignKey(Receipt.id), nullable=False)
+    product_id = Column(Integer, ForeignKey(Product.id), nullable=False)
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        import hashlib
-
-        u = User(name='Admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), user_role=UserRoleEnum.ADMIN)
-        db.session.add(u)
-        db.session.commit()
-        c1 = Category(name='Mobile')
-        c2 = Category(name='Tablet')
-        c3 = Category(name='Desktop')
-        db.session.add(c1)
-        db.session.add(c2)
-        db.session.add(c3)
-        db.session.commit()
-
-        p1 = Product(name='iPhone 13', price=20000000, category_id=1)
-        p2 = Product(name='Galaxy S23', price=24999999, category_id=1)
-        p3 = Product(name='Ipad', price=45300000, category_id=2)
-        p4 = Product(name='MSI', price=32300000, category_id=3)
-        p5 = Product(name='Oppo N3 Flip', price=20333000, category_id=1)
-        p6 = Product(name='Galaxy Tab', price=1203000, category_id=2)
-        db.session.add(p1)
-        db.session.add(p2)
-        db.session.add(p3)
-        db.session.add(p4)
-        db.session.add(p5)
-        db.session.add(p6)
-        db.session.commit()
+        # import hashlib
+        #
+        # u = User(name='Admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), user_role=UserRoleEnum.ADMIN)
+        # db.session.add(u)
+        # db.session.commit()
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        # c3 = Category(name='Desktop')
+        # db.session.add(c1)
+        # db.session.add(c2)
+        # db.session.add(c3)
+        # db.session.commit()
+        #
+        # p1 = Product(name='iPhone 13', price=20000000, category_id=1)
+        # p2 = Product(name='Galaxy S23', price=24999999, category_id=1)
+        # p3 = Product(name='Ipad', price=45300000, category_id=2)
+        # p4 = Product(name='MSI', price=32300000, category_id=3)
+        # p5 = Product(name='Oppo N3 Flip', price=20333000, category_id=1)
+        # p6 = Product(name='Galaxy Tab', price=1203000, category_id=2)
+        # db.session.add(p1)
+        # db.session.add(p2)
+        # db.session.add(p3)
+        # db.session.add(p4)
+        # db.session.add(p5)
+        # db.session.add(p6)
+        # db.session.commit()
--- SaleApp/app/static/js/cart.js
+++ SaleApp/app/static/js/cart.js
@@ -18,3 +18,56 @@ function addToCart(id, name, price) {
         }
     })
 }
+
+function updateCart(id, obj) {
+    obj.disable = true
+    fetch(`/api/cart/${id}`,  {
+        method: "put",
+        body: JSON.stringify({
+            'quantity': obj.value
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function(res) {
+        return res.json()
+    }).then(function(data) {
+        obj.disable = false
+
+        let carts = document.getElementsByClassName("cart-counter");
+        for (let c of carts) {
+            c.innerText = data.total_quantity;
+        }
+
+        let amounts = document.getElementsByClassName("cart-amount");
+        for (let c of amounts) {
+            c.innerText = data.total_amount.toLocaleString('en');
+        }
+    })
+}
+
+function deleteCart(id, obj) {
+    if (confirm("Bạn chắc chắn xóa") === true) {
+        obj.disable = true
+        fetch(`/api/cart/${id}`,  {
+            method: "delete"
+        }).then(function(res) {
+            return res.json()
+        }).then(function(data) {
+            obj.disable = false
+
+            let carts = document.getElementsByClassName("cart-counter");
+            for (let c of carts) {
+                c.innerText = data.total_quantity;
+            }
+
+            let amounts = document.getElementsByClassName("cart-amount");
+            for (let c of amounts) {
+                c.innerText = data.total_amount.toLocaleString('en');
+            }
+
+            let t = document.getElementById(`product${id}`)
+            t.style.display = 'none'
+        })
+    }
+}
